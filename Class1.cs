b6bcff01f4f3f22f9a5962e8eeb06308fd5c42eb
Val-in <valeriia.vp1@yandex.ru>
2024-07-01 19:21:18 +0800

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿using Task2;
+
+namespace Task2
+{
+    public class Worker1 : IWorker
+    {
+        private readonly ILogger _logger;
+
+        public Worker1(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Work()
+        {
+            _logger.Event("Worker 1 started");
+            Thread.Sleep(3000);
+            _logger.Event("Worker 1 stopped");
+        }
+    }
+
+    public class Worker2 : IWorker
+    {
+        private readonly ILogger _logger;
+
+        public Worker2(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Work()
+        {
+            _logger.Event("Worker 2 started");
+            Thread.Sleep(3000);
+            _logger.Event("Worker 2 stopped");
+        }
+    }
+
+    public class Worker3 : IWorker
+    {
+        private readonly ILogger _logger;
+
+        public Worker3(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Work()
+        {
+            _logger.Event("Worker 3 started");
+            Thread.Sleep(3000);
+            _logger.Event("Worker 3 stopped");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using static Task2.Program;
+
+namespace Task2
+{
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Event(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public void Error(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+
+    public interface IWorker
+    {
+        void Work();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new Logger();
+            var worker1 = new Worker1(logger);
+            var worker2 = new Worker2(logger);
+            var worker3 = new Worker3(logger);
+
+            worker1.Work();
+            worker2.Work();
+            worker3.Work();
+
+            Console.WriteLine("All work completed.");
+        }
+    }
+}
